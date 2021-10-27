a562159bc23eb0b63b9d3f2a13ee06f4f44f51f7
Mikhail Pasichniuk <michael.reynmar@gmail.com>
2021-10-27 14:52:36 +0300

    nav toggle is somehow broken

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,9 +17,15 @@ const SidebarStyled = styled.div`
     z-index: 2;
     background-color: var(--sidebar-dark-color);
     transition: all .4s ease-in-out;
-
+    
+    .nav-toggle{
+        transform: translateX(0);
+    }
     @media screen and (max-width: 1080px){
         transform: translateX(-100%);
+        .nav-toggle{
+            transform: translateX(-100%);
+        }
     }
 
 `;
