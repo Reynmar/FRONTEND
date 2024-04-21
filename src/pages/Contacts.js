c46611000f9c6b5d4bd4873ec1b1519c7f68a8ca
Mikhail <maikl@Mikhails-MacBook-Pro.local>
2024-04-21 14:31:38 -0400

    remove unrelevant contact info

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,154 +1,107 @@
-import React from 'react';
-import styled from 'styled-components';
-import Particle from '../components/particles';
-import FacebookIcon from '@material-ui/icons/Facebook'
-import GithubIcon from '@material-ui/icons/GitHub'
-import YoutubeIcon from '@material-ui/icons/YouTube'
-import TelegramIcon from '@mui/icons-material/Telegram';
-import AttachEmailIcon from '@mui/icons-material/AttachEmail';
-import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import React from "react";
+import styled from "styled-components";
+import Particle from "../components/particles";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GithubIcon from "@material-ui/icons/GitHub";
+import YoutubeIcon from "@material-ui/icons/YouTube";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import AttachEmailIcon from "@mui/icons-material/AttachEmail";
+import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const Contacts = () => {
-    return (
-        <ContactsStyled>
-            <div className="p-particles-js">
-                <Particle />
-            </div>
+  return (
+    <ContactsStyled>
+      <div className="p-particles-js">
+        <Particle />
+      </div>
 
-            <div className="typography">
-                <h1>
-                    Mikhail Pasichniuk <span> contacts</span>
-                </h1>
-                <p>
-                    You are more then welcome to reach me using e-mail,
-                    telegram, or any of the Social Networks available below{" "}
-                </p>
+      <div className="typography">
+        <h1>
+          Mikhail Pasichniuk <span> contacts</span>
+        </h1>
+        <p>You can reach me using any of the Social Networks available below</p>
 
-                {/* <div className="contact-info">
-                    <FacebookIcon/>
-                    <h2>mpasichnyuk@gmail.com</h2>
-                </div>
-                <div className="contact-info">
-                    <FacebookIcon/>
-                    <h2>+79999730731</h2>
-                </div> */}
-
-                <div className="icons">
-                    <a
-                        href="https://github.com/mpasichnyuk"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="icon i-github"
-                    >
-                        <GithubIcon />
-                    </a>
-                    <a
-                        href="https://www.facebook.com/michaelreynmar"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="icon i-facebook"
-                    >
-                        <FacebookIcon />
-                    </a>
-                    <a
-                        href="https://www.youtube.com/channel/UCL8LPGjXjvkc0dWGIJic7hQ"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="icon i-youtube"
-                    >
-                        <YoutubeIcon />
-                    </a>
-                    <a
-                        href="https://www.instagram.com/michael_reynmar/"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="icon i-youtube"
-                    >
-                        <InstagramIcon />
-                    </a>
-
-                    <a
-                        href="mailto:mpasichnyuk@gmail.com"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="icon i-youtube"
-                    >
-                        <MarkEmailReadIcon />
-                    </a>
-
-                    <a
-                        href="https://t.me/MichaelReynmar"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="icon i-youtube"
-                    >
-                        <TelegramIcon />
-                    </a>
-                </div>
-            </div>
-        </ContactsStyled>
-    );
-}
+        <div className="icons">
+          <a
+            href="https://github.com/mpasichnyuk"
+            target="_blank"
+            rel="noreferrer"
+            className="icon i-github"
+          >
+            <GithubIcon />
+          </a>
+          <a
+            href="https://www.facebook.com/michaelreynmar"
+            target="_blank"
+            rel="noreferrer"
+            className="icon i-facebook"
+          >
+            <FacebookIcon />
+          </a>
+        </div>
+      </div>
+    </ContactsStyled>
+  );
+};
 
 const ContactsStyled = styled.header`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+
+  .contact-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  p {
+    margin-top: 1rem;
+  }
+
+  .p-particles-js {
     width: 100%;
-    height: 100vh;
-    position: relative;
-
-    .contact-info{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    p{
-        margin-top: 1rem;
-    }
-
-    .p-particles-js{
-        width: 100%;
-        z-index: -2;
-        /* position: absolute;
+    z-index: -2;
+    /* position: absolute;
         top: 0;
         left: 0; */
-    }
+  }
 
-    .typography{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%,-50%);
-        text-align: center;
-        width: 70%;
-        
-        .icons {
-            display: flex;
-            justify-content: center;
-            flex-wrap: wrap;
-            margin-top: 1rem;
-            .icon{
-                border: 2px solid var(--border-color);
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                border-radius: 50%;
-                cursor: pointer;
-                transition: all .2s ease-in-out;
+  .typography {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 70%;
 
-                &:hover{
-                    background-color: var(--primary-color);
-                }
-                &:not(:last-child){
-                    margin-right: 1rem;
-                }
-                svg{
-                    margin: .5rem;
-                }
-            }
+    .icons {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+      margin-top: 1rem;
+      .icon {
+        border: 2px solid var(--border-color);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: all 0.2s ease-in-out;
+
+        &:hover {
+          background-color: var(--primary-color);
         }
+        &:not(:last-child) {
+          margin-right: 1rem;
+        }
+        svg {
+          margin: 0.5rem;
+        }
+      }
     }
+  }
 `;
-
 
 export default Contacts;
